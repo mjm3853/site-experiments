186411b982da1ff465cdf6c5e1c95141c37f5f92
Matthew McGrath <matthew.mcgrath12@gmail.com>
2018-08-20 20:20:03 -0400

    feat(sw) mostly working service worker. timestamps to cache bust

---
+++ src/register-serviceworker.js
@@ -0,0 +1,9 @@
+if ('serviceWorker' in navigator) {
+  // Use the window load event to keep the page load performant
+  window.addEventListener('load', () => {
+    navigator.serviceWorker
+      .register('./sw.js')
+      .then()
+      .catch(err => console.log(err));
+  });
+}
