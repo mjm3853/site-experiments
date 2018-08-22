1c6df536765a2911163e8b3360b7995f91f1a199
Matthew McGrath <matthew.mcgrath12@gmail.com>
2018-08-21 21:14:53 -0400

    remove service worker complexity

--- src/register-serviceworker.js
+++
@@ -1,8 +0,0 @@
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', () => {
-    navigator.serviceWorker
-      .register('./serviceworker.js')
-      .then()
-      .catch(err => console.log(err));
-  });
-}
