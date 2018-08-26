4939a62f6f9af666da133373ccd72f22f5295216
Matthew McGrath <matthew.mcgrath12@gmail.com>
2018-08-25 20:08:37 -0400

    non-scalable router with lit-html

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -1,6 +1,21 @@
-// DELETE ME when creating a site
-document.addEventListener("DOMContentLoaded", () => {
-    document.getElementById("typescript-check").innerHTML = "TypeScript Working";
+import {html, render} from "lit-html";
+
+// Templates
+const helloWorld = html`<p>Hello World</p>`;
+const samplePage = html`<p>Sample Page</p>`;
+
+// Initial Page Content
+render(helloWorld, document.getElementById("tmp-content"));
+
+// Navigation Handlers
+document.getElementById("home-page").addEventListener("click", () => {
+    render(helloWorld, document.getElementById("tmp-content"));
+    history.pushState(null, "Home Page", "index.html");
+});
+
+document.getElementById("sample-page").addEventListener("click", () => {
+    render(samplePage, document.getElementById("tmp-content"));
+    history.pushState(null, "Sample Page", "sample-page.html");
 });
 
 // DELETE ME when creating a site
