65aedd44fc038d236dee95a5cb86785bab332e54
Matthew McGrath <matthew.mcgrath12@gmail.com>
2018-09-15 15:29:22 -0400

    inline slide entrance animation

--- src/ts/pages/home.page.ts
+++ src/ts/pages/home.page.ts
@@ -1,6 +1,29 @@
+import { html } from "lit-html";
 import { IPageData } from "../models/page.model";
 
 export const homePage: IPageData = {
     title: "Home Page!!!",
-    body: "This is the home page",
+    body: html`
+    <style>
+        .tmp-animate {
+            animation-duration: 400ms;
+            animation-name: slideUp;
+            animation-timing-function: ease-in;
+        }
+
+        @keyframes slideUp {
+            from {
+                opacity: 0;
+                margin-top: 20%;
+            }
+
+            to {
+                opacity: 1;
+                margin-top: 0%;
+            }
+        }
+    </style>
+    <p class="tmp-animate">This is the home page</p>
+    <p class="tmp-animate">I will be animated</p>
+    `,
 };
