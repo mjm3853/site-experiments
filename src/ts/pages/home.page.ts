dbe92f28ee70a641fad589395de4ebe18dff79d6
Matthew McGrath <matthew.mcgrath12@gmail.com>
2018-10-20 20:35:45 -0400

    random updates

--- src/ts/pages/home.page.ts
+++ src/ts/pages/home.page.ts
@@ -3,37 +3,23 @@ import { imageObjectTemplate } from "../models/imageObject.model";
 import { IPageData } from "../models/page.model";
 
 import { anotherArticle } from "../articles/another.article";
+import { sampleArticle } from "../articles/sample.article";
 import { articleTemplate } from "../models/article.model";
 
 // @ts-ignore
 import babyBear from "../../assets/baby-bear1.jpg";
+// @ts-ignore
+import tsLogo from "../../assets/ts.png";
 
 export const homePage: IPageData = {
     title: "Home Page!!!",
     body: html`
-    <style>
-        .tmp-animate {
-            animation-duration: 400ms;
-            animation-name: slideUp;
-            animation-timing-function: ease-in;
-        }
-
-        @keyframes slideUp {
-            from {
-                opacity: 0;
-                margin-top: 30%;
-            }
-
-            to {
-                opacity: 1;
-                margin-top: 0%;
-            }
-        }
-    </style>
-    <p class="tmp-animate">This is the home page</p>
-    <p class="tmp-animate">I will be animated</p>
     ${imageObjectTemplate({
         imagePath: babyBear,
+        body: html`${articleTemplate(sampleArticle)}`,
+    })}
+    ${imageObjectTemplate({
+        imagePath: tsLogo,
         body: html`${articleTemplate(anotherArticle)}`,
     })}
     `,
